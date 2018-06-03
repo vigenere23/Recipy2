f39149b8a4c07ee99034990fda77ff22bb7952c8
vigenere <lolgab1@hotmail.com>
2018-06-02 20:44:56 -0400

    modified scripts to better support asynchronous functions and promises

--- API/scripts/populateDB.js
+++ API/scripts/populateDB.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
 import consts from '@/constants'
-
 import user from '@/model_templates/user'
 import recipe from '@/model_templates/recipe'
 import comment from '@/model_templates/comment'
@@ -8,20 +7,30 @@ import comment from '@/model_templates/comment'
 comment.recipesID = recipe._id
 recipe.userID = user._id
 
-mongoose.connect(consts.DB_PATH).then(result => {
+mongoose.connect(consts.DB_PATH).then(async () => {
+
   console.log('Connected to database')
-  console.log('Beggining populating DB...')
+  console.log('Beggining populating DB with model templates\n...')
+
   for (var i = 0; i < 5; i++) {
     recipe.commentsID.push(comment._id)
     user.recipesID.push(recipe._id)
   }
-  comment.save().then(() => {
-    recipe.save().then(() => {
-      user.save()
-    })
-  })
-}).then(() => {
-  console.log("DONE")
-}).catch(err => {
-  throw err
+
+  try {
+    await comment.save()
+    await recipe.save()
+    await user.save()
+    console.log('DONE!')
+  }
+  catch (err) {
+    console.log('ERR: Could not save a collection')
+    console.error(err)
+  }
+
+}, err => {
+
+  console.log('ERR: Could not connect to database (probably wrong URL)')
+  console.error(err)
+
 })
--- API/scripts/resetDB.js
+++ API/scripts/resetDB.js
@@ -1,23 +1,36 @@
 import mongoose from 'mongoose'
-
 import consts from '@/constants'
 
-mongoose.connect(consts.DB_PATH).then(result => {
+
+mongoose.connect(consts.DB_PATH).then(async () => {
+
   console.log('Connected to database')
-  mongoose.connection.db.listCollections().toArray().then(collections => {
-    if (collections.length != 0) {
-      console.log('Beginning deletion of ALL model entries...')
-      collections.forEach(collection => {
-        mongoose.connection.dropCollection(collection.name)
-        console.log('DELETED \'' + collection.name + '\' collection')
+  
+  var collections = await mongoose.connection.db.listCollections().toArray()
+
+  if (collections.length != 0) {
+
+    console.log(`Beginning deletion of ALL model entries (${collections.length})\n...`)
+
+    for (const collection of collections) {
+      await mongoose.connection.dropCollection(collection.name).then(() => {
+        console.log("DELETED '" + collection.name + "' collection")
+      }, err => {
+        console.log("Could not drop collection '" + collection.name + "'")
+        console.error(err)
       })
-      console.log('DONE')
     }
-    else {
-      console.log('Database already cleaned')
-    }
-  })
-}).catch(err => {
-  console.log('OOPS, SOMETHING WENT WRONG!')
-  throw error
+
+    console.log('DONE')
+    
+  }
+  else {
+    console.log('Database already cleaned!')
+  }
+
+}, err => {
+
+  console.log('Could not connect to database (probably wrong URL)')
+  console.error(err)
+
 })
