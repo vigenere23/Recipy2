ed8ea47638b3740c1ddf30a37511ab13bb44313f
vigenere <lolgab1@hotmail.com>
2018-06-18 22:26:42 -0400

    added singleRecipe vue

--- UI/src/routes.js
+++ UI/src/routes.js
@@ -5,18 +5,17 @@ import DefaultLayout from '@/layouts/Default.vue'
 // Pages
 import FindRecipesPage from '@/pages/FindRecipes.vue'
 import ExploreRecipesPage from '@/pages/ExploreRecipes.vue'
+import SingleRecipePage from '@/pages/SingleRecipe.vue'
 
 const routes = [
 	{ path: '/', redirect: '/recipes' },
-
-	// { path: '*/new', component: ReducedLayout },
 
 	{ path: '*', component: DefaultLayout, children: [
 
 		{ path: 'recipes', redirect: '/recipes/find' },
     { path: 'recipes/find', component: FindRecipesPage },
     { path: 'recipes/explore', compoonent: ExploreRecipesPage },
-		// { path: 'recipes/:id', component: SingleRecipePage } 
+		{ path: 'recipes/:id', component: SingleRecipePage } 
 
 	] }
 ]
